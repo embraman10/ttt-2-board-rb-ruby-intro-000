3dd9526f02f43b5854bdd26972367f54ca31ebaa
Emily Braman <embraman10@gmail.com>
2020-05-25 22:31:27 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,1 @@
 board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-# Position 1: top left
-board [ ]
-# Position 2: top middle
-board [ ]
-# Position 3: top right
-board [ ]
-#Position 4: middle left
-board [ ]
-# Position 5: middle
-board [ ]
-# Position 6: middle right
-board [ ]
-# Position 7: bottom left
-board [ ]
-# Position 8: bottom middle
-board [ ]
-# Position 9: bottom right
-board [ ]
