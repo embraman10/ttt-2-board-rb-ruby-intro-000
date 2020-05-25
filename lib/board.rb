c8c33d79fff20d6a0d9f4a902e930cf5acd7f93a
Emily Braman <embraman10@gmail.com>
2020-05-25 22:27:57 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,19 @@
-# Define the variable board below.
-row = ["1", "2", "3"]
-# Position 1: left
-row [0]
-# Position 2: middle
-row [1]
-# Poistion 3: right
-row [2]
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# Position 1: top left
+board [0]
+# Position 2: top middle
+board [1]
+# Position 3: top right
+board [2]
+#Position 4: middle left
+board [3]
+# Position 5: middle
+board [4]
+# Position 6: middle right
+board [5]
+# Position 7: bottom left
+board [6]
+# Position 8: bottom middle
+board [7]
+# Position 9: bottom right
+board [8]
